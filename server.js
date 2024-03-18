23232ca287cbaee847e60e0ddf855286f43629df
Chelle Wood <chelleyoungw@gmail.com>
2024-03-17 20:17:03 -0500

    in server.js file created dependency for express, to use express, and create a port

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+// Dependencies
+const express = require('express');
+
+// Asks the app to use Express.js and creates the variable port that the server runs on
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 
