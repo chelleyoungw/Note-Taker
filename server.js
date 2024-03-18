7825b4896280c48de95e1e2f3e9d92047ab45b9c
Chelle Wood <chelleyoungw@gmail.com>
2024-03-17 20:33:36 -0500

    added middleware for app to point to the public folder for static files

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware that points to the public folder
+app.use(express.static('public'));
+
+
 // app listener, to start the server on the specified PORT
 app.listen(PORT, () => {
     console.log('App listening at http://localhost:${PORT}')
