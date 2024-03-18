6427863e333baef03a4b67ec6db7b283825856a8
Chelle Wood <chelleyoungw@gmail.com>
2024-03-17 20:24:59 -0500

    added app listener

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+// app listener, to start the server on the specified PORT
+app.listen(PORT, () => {
+    console.log('App listening at http://localhost:${PORT}')
+});
