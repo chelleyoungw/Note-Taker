2a28ce30d85fe324ce224924a528277d3b2a2ff0
Chelle Wood <chelleyoungw@gmail.com>
2024-03-17 20:44:51 -0500

    added middleware to parse incoming requests for json and bodies encoded in URL-encoded format

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3001;
 // Middleware that points to the public folder
 app.use(express.static('public'));
 
+// Middleware to parse incoming request bodies
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 // app listener, to start the server on the specified PORT
 app.listen(PORT, () => {
