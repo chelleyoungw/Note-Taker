64654811f44cb4ae402744f59e44227903ab388c
Chelle Wood <chelleyoungw@gmail.com>
2024-03-19 21:23:06 -0500

    added get request in htmlroutes.js to return notes and index.html

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+// Dependencies
+const path = require('path');
+
+// Routing
+module.exports = (app) => {
+
+    // returns notes.html
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
+    // returns index.html
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+};
