f114a33eb0d9e45f9ddc24cfc8a5e73a980a9251
Chelle Wood <chelleyoungw@gmail.com>
2024-03-19 21:17:54 -0500

    added GET and POST to read and save new notes to the db.json file and creates a unique id for each new notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,3 +4,32 @@ const fs = require('fs');
 
 // npm pkg that allows unique ids for notes to be created to save them individually
 var uniqueId = require('uniqId');
+
+// Routing
+module.exports = (app) => {
+
+    // returns saved notes as JSON
+    app.get('/api/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../db/db.json'));
+    });
+    
+    // takes a new note, saves, then returns a new one
+    app.post('/api/notes', (req, res) => {
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+
+        // creates note
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id:uniqueId(),
+        };
+
+        // sends note to db.json file
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+
+    });
+}
